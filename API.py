f44040c8a33148295153fcb9c5466cbd357fa38f
amleman <martin1aleman@gmail.com>
2025-01-13 22:55:18 -0600

    api horario

--- API.py
+++ API.py
@@ -131,4 +131,5 @@ def ganados():
                 return "400"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
